150ef53d9c84233a92bfa23e1dc9f6966099599c
Mohan Dhakal <mohandkl.512@gmail.com>
2019-01-25 23:50:17 +0545

    some changes in MainActivity and xml files

--- AgroProject/app/src/main/java/com/ourproject/mohankumardhakal/agroproject/MainActivity.java
+++ AgroProject/app/src/main/java/com/ourproject/mohankumardhakal/agroproject/MainActivity.java
@@ -1,13 +1,25 @@
 package com.ourproject.mohankumardhakal.agroproject;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.post_sample);
+
+    }
+
+    public void showPopUp(View v) {
+        Dialog dialog = new Dialog(this, R.style.Dialog);
+        dialog.setTitle("Request Here");
+        View view = LayoutInflater.from(this).inflate(R.layout.product_request_dialog, null);
+        dialog.setContentView(view);
+        dialog.show();
     }
 }

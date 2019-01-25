051bae14193fecba895e803e63a30b7b1b48bdfb
Mohan Dhakal <mohandkl.512@gmail.com>
2019-01-25 19:04:12 +0545

    Main class added

--- AgroProject/app/src/main/java/com/ourproject/mohankumardhakal/agroproject/RandomClass.java
+++ AgroProject/app/src/main/java/com/ourproject/mohankumardhakal/agroproject/RandomClass.java
@@ -1,4 +1,7 @@
 package com.ourproject.mohankumardhakal.agroproject;
 
 public class RandomClass {
+    public static void main(String[] args) {
+        System.out.println("Testing Github for now");
+    }
 }

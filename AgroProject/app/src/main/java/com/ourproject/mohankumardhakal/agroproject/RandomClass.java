884303052270b8c1d4e0ba270dba885205718f35
Mohan Dhakal <mohandkl.512@gmail.com>
2019-01-25 19:09:54 +0545

    Unwanted file for checking has been deleted

--- AgroProject/app/src/main/java/com/ourproject/mohankumardhakal/agroproject/RandomClass.java
+++
@@ -1,7 +0,0 @@
-package com.ourproject.mohankumardhakal.agroproject;
-
-public class RandomClass {
-    public static void main(String[] args) {
-        System.out.println("Testing Github for now");
-    }
-}

774d4da32dd36693afcaba4f2aadf7719c7c0d34
Mohan Dhakal <mohandkl.512@gmail.com>
2019-01-26 14:43:45 +0545

    added piechart libraries

--- AgroProject/app/src/main/java/com/ourproject/mohankumardhakal/agroproject/FragmentClasses/FarmersPostFrame.java
+++ AgroProject/app/src/main/java/com/ourproject/mohankumardhakal/agroproject/FragmentClasses/FarmersPostFrame.java
@@ -1,5 +1,4 @@
 package com.ourproject.mohankumardhakal.agroproject.FragmentClasses;
-
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -8,18 +7,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.ourproject.mohankumardhakal.agroproject.R;
-
 public class FarmersPostFrame extends Fragment {
     RecyclerView recyclerView;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.recyclerview_for_post, null);
         recyclerView = view.findViewById(R.id.recyclerView);
-system.out.println("This is the remote changes i made on my another github");
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 }

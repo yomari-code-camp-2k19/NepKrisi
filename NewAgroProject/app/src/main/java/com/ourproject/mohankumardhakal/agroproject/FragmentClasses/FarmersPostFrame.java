11966b8db7bf28561d855f9820f410d2cc8dba87
Mohan Dhakal <mohandkl.512@gmail.com>
2019-01-29 10:11:31 +0545

    merge changed without actual java classes in activity package

---
+++ NewAgroProject/app/src/main/java/com/ourproject/mohankumardhakal/agroproject/FragmentClasses/FarmersPostFrame.java
@@ -0,0 +1,20 @@
+package com.ourproject.mohankumardhakal.agroproject.FragmentClasses;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.ourproject.mohankumardhakal.agroproject.R;
+public class FarmersPostFrame extends Fragment {
+    RecyclerView recyclerView;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.recyclerview_for_post, null);
+        recyclerView = view.findViewById(R.id.recyclerView);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
